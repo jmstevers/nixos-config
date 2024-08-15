4ea4b1831ce14d69cec834b508288f938d5fa1d6
Johnathan Stevers <jmstevers@gmail.com>
2024-08-15 01:34:55 -0500

    game optimization

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -40,6 +40,7 @@
 
     system.stateVersion = "24.05";
 
+    boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
@@ -82,8 +83,55 @@
     services.displayManager.sddm.wayland.enable = true;
 
     programs.steam.enable = true;
+
     programs.gamescope = {
         enable = true;
         capSysNice = true;
+    };
+
+    hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+            rocmPackages.clr.icd
+        ];
+    };
+
+    environment.variables = {
+        ROC_ENABLE_PRE_VEGA = "1";
+    };
+
+    systemd.tmpfiles.rules = let
+        rocmEnv = pkgs.symlinkJoin {
+            name = "rocm-combined";
+            paths = with pkgs.rocmPackages; [
+                rocblas
+                hipblas
+                clr
+            ];
+        };
+    in [
+        "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+    ];
+
+    services.postgresql = {
+        enable = true;
+        ensureDatabases = [ "teach_dev" ];
+        enableTCPIP = true;
+        settings.port = 5432;
+        authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  auth-method
+            local all       all     trust
+            #type database DBuser origin-address auth-method
+            # ipv4
+            host  all      all     127.0.0.1/32   trust
+            # ipv6
+            host all       all     ::1/128        trust
+        '';
+        initialScript = pkgs.writeText "backend-initScript" ''
+            CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+            CREATE DATABASE nixcloud;
+            GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+        '';
     };
 }
