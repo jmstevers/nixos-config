eae012b12955cc2b3d78dc5e1240e446e5d0645b
Johnathan Stevers <jmstevers@gmail.com>
2024-10-19 09:11:29 -0500

    clean up audio file

--- packages/audio.nix
+++ packages/audio.nix
@@ -1,21 +1,15 @@
 {
   security.rtkit.enable = true;
-  services = {
-    pipewire = {
-      enable = true;
-      pulse.enable = true;
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      extraConfig.pipewire."92-low-latency" = {
-        context.properties = {
-          default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 32;
-        };
-      };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+    extraConfig.pipewire."92-low-latency".context.properties.default.clock = {
+      rate = 48000;
+      quantum = 32;
+      min-quantum = 32;
+      max-quantum = 32;
     };
   };
 }
